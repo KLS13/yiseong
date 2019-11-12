8a80c13f8aa665607faeb8f782d3881269b878c8
Kang Yi Seong <54849894+KLS13@users.noreply.github.com>
2019-11-12 18:08:39 +0900

    2019년 11월

---
+++ 2019.11/2019.11.12/src/tostring/SmartPhoneExample.java
@@ -0,0 +1,12 @@
+package tostring;
+
+public class SmartPhoneExample {
+	public static void main(String[] args) {
+		SmartPhone myPhone = new SmartPhone("구글", "안드로이드");
+		
+		String strObj = myPhone.toString();
+		System.out.println(strObj);
+		
+		System.out.println(myPhone);
+	}
+}
